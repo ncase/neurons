7db833f903526d2cbd99847b00f81c624670b4d4
Nicky Case <n@ncase.me>
2015-09-15 13:38:05 -0700

    Volume and Captions file

--- scripts/core/Controls.js
+++ scripts/core/Controls.js
@@ -21,4 +21,51 @@ subscribe("/mouse/down",function(){
 	}
 });
 
+//////////////////////
+/// VOLUME CONTROL ///
+//////////////////////
+
+var volumeIcon = document.getElementById("control_volume");
+var volumeSlider = document.getElementById("control_volume_slider");
+
+var _lastVolume = 1;
+volumeIcon.onclick = function(){
+
+	// Edge Case - muting when already muted
+	if(!createjs.Sound.muted && createjs.Sound.volume==0){
+		_lastVolume = 1;
+		createjs.Sound.volume = volumeSlider.value = 1;
+		_updateVolumeIcon();
+		return;
+	}
+
+	// Otherwise, toggle between mute & last volume.
+	createjs.Sound.muted = !createjs.Sound.muted;
+	if(createjs.Sound.muted){
+		_lastVolume = volumeSlider.value;
+		volumeSlider.value = 0;
+	}else{
+		createjs.Sound.volume = volumeSlider.value = _lastVolume;
+	}
+	_updateVolumeIcon();
+
+};
+
+volumeSlider.oninput = function(){
+	createjs.Sound.muted = false;
+	createjs.Sound.volume = volumeSlider.value;
+	_updateVolumeIcon();
+};
+
+var _updateVolumeIcon = function(){
+	var state = 0;
+	if(createjs.Sound.muted || createjs.Sound.volume==0){
+		state = 0;
+	}else{
+		state = Math.ceil(createjs.Sound.volume*3);
+	}
+	state = 3-state;
+	volumeIcon.style.backgroundPosition = (-state*47)+"px 0px";
+};
+
 })();
